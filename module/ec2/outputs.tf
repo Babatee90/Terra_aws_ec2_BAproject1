b1c29b4ec93bc4deb189bc6d414ee96f0052d81d
Babatee90 <125268411+Babatee90@users.noreply.github.com>
2026-02-05 08:06:22 +0000

    Enhance instance_ids output with description
    Add description for instance_ids output

--- module/ec2/outputs.tf
+++ module/ec2/outputs.tf
@@ -3,6 +3,7 @@ output "ami_id_used" {
   value       = var.ami_id
 }
 output "instance_ids" {
+description = "The instance id used for the EC2 instances
   value = aws_instance.web_server[*].id
 }
 
