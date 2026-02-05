c7cfd0fb0d2a47435c19f83a90aad2ba488fef22
Babatee90 <125268411+Babatee90@users.noreply.github.com>
2026-02-05 09:57:16 +0000

    removed duplicate output.tf

--- module/outputs.tf
+++ module/outputs.tf
@@ -1,12 +1,1 @@
-output "ami_id_used" {
-  description = "The AMI ID used for the EC2 instances"
-  value       = var.ami_id
-}
-output "instance_ids" {
-  value = aws_instance.web_server[*].id
-}
 
-
-output "ami_id" {
-  value = var.ami_id
-}
